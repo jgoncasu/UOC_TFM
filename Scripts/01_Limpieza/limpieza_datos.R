9e1ef408b63722d92750ab05fe6f4094aa9007d9
Jorge González del Castillo <jgoncasu@gmail.com>
2024-11-07 13:15:39 +0000

    Esquema script limpieza

---
+++ Scripts/01_Limpieza/limpieza_datos.R
@@ -0,0 +1,12 @@
+# Librerías
+
+# Crear lista de barrios de Londres y sus códigos en fichero
+# Cargar barrios y crear una lista
+# Carga ficheros
+# Elimina columnas
+# Filtra filas (solo barrios y datos de la ciudad de Londres)
+# Revisión separadores decimales
+# Revisión tipo de los datos
+# Sustituir valores no válidos
+# Imputar valores nulos
+# Revisar todos los barrios tienen datos de 2001, 2011, 2021 y 2031 (datos por censo)
