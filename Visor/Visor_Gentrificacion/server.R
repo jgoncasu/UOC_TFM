b4a038b4eeb08a2e173f6f4596d827e062837866
Jorge González del Castillo <jgoncasu@gmail.com>
2024-11-23 19:01:42 +0000

    Visor

--- Visor/Visor_Gentrificacion/server.R
+++ Visor/Visor_Gentrificacion/server.R
@@ -21,15 +21,9 @@ carga_datos_brutos <- function() {
   ruta_fichero <- 'DAT_Brutos_Londres.csv'
   df = read_csv(paste(PATH_FICHEROS_DATOS, ruta_fichero, sep=""), col_types = list(col_character(), col_character(), col_integer(),
                                                                                    col_double(), col_double(), col_double(),
-                                                                                   col_integer(), col_double(), col_double(),
-                                                                                   col_double(), col_double(), col_double(),
-                                                                                   col_double(), col_double(), col_double(),
-                                                                                   col_integer(), col_double(), col_double(),
-                                                                                   col_double(), col_double(), col_double(),
-                                                                                   col_double(), col_double(), col_double(),
-                                                                                   col_integer(), col_double(), col_double(),
-                                                                                   col_double(), col_double(), col_double(),
-                                                                                   col_double(), col_double(), col_double()
+                                                                                   col_double(), col_integer(), col_double(),
+                                                                                   col_double(), col_integer(), col_double(),
+                                                                                   col_double()
   ))
   return(df)
 }
@@ -59,25 +53,47 @@ carga_indicadores <- function() {
 ################################################################################
 df_indicadores <- carga_indicadores()
 df_datos_brutos <- carga_datos_brutos()
-#df_londres_brutos <- df_datos_brutos %>% filter(BOROUGH == "London")
-#df_datos_brutos <- df_datos_brutos %>% filter(BOROUGH != "London")
 
 ################################################################################
 # Lógica de la aplicación
 ################################################################################
 function(input, output, session) {
   
+  ################################################################################
+  # Carga de datos
+  ################################################################################
+  carga_indicadores_londres_anyos <- reactive({
+    # Filtra por años
+    df <- df_indicadores %>% filter(YEAR == input$sldYear & BOROUGH == "London")
+    df <- df %>% select(-starts_with("IND_"))
+    colnames(df) <- c("Codigo", "Barrio", "Año", "Edad", "Var01", "Pob_Blanca", "Var02", "Salario", "Var03", "Estudios", "Var04", "Trafico", "Var05", "Esp_Vida", "Var06", "Delitos", "Var07", "Servicios", "Var08", "Pr_Vivienda", "Var09", "Alquiler", "Var10")
+    return(df)
+  })
+  
+  carga_indicadores_anyos <- reactive({
+    # Filtra por años
+    df <- df_indicadores %>% filter(YEAR == input$sldYear & BOROUGH != "London")
+    df <- df %>% select(-starts_with("IND_")) %>% arrange(BOROUGH)
+    colnames(df) <- c("Codigo", "Barrio", "Año", "Edad", "Var01", "Pob_Blanca", "Var02", "Salario", "Var03", "Estudios", "Var04", "Trafico", "Var05", "Esp_Vida", "Var06", "Delitos", "Var07", "Servicios", "Var08", "Pr_Vivienda", "Var09", "Alquiler", "Var10")
+    return(df)
+  })
+  
+  carga_datos_brutos_anyos <- reactive({
+    # Filtra por años
+    df <- df_datos_brutos %>% filter(YEAR <= input$sldYear & YEAR >= (input$sldYear - 10))
+    df <- df %>% arrange(BOROUGH)
+    colnames(df) <- c("Codigo", "Barrio", "Año", "Edad", "Pob_Blanca", "Salario", "Estudios", "Trafico", "Esp_Vida", "Delitos", "Servicios", "Pr_Vivienda", "Alquiler")
+    return(df)
+  })
+  
   carga_datos_brutos_barrios <- reactive({
     # Filtra por años
     df <- df_datos_brutos %>% filter(YEAR <= input$sldYear & YEAR >= (input$sldYear - 10))
-    
     # Filtra por barrios
     if (length(input$selBorough) > 0)
       df <- df %>% filter(CODE %in% input$selBorough)
-    
     df <- df %>% arrange(BOROUGH)
     colnames(df) <- c("Codigo", "Barrio", "Año", "Edad", "Pob_Blanca", "Salario", "Estudios", "Trafico", "Esp_Vida", "Delitos", "Servicios", "Pr_Vivienda", "Alquiler")
-    
     return(df)
   })
 
@@ -94,80 +110,162 @@ function(input, output, session) {
     
     return(df)
   })
+
+  ################################################################################
+  # Explorador de indicadores
+  ################################################################################
+  # Tabla del explorador de indicadores
+#  output$tblIndicadoresLondres <- DT::renderDT(
+#      expr = carga_indicadores_londres_anyos(), rownames = FALSE, options = list(scrollX = TRUE,
+#                                                                                 dom = 't',
+#                                                                                 columnDefs = list(list(visible = FALSE, targets = c(0))))
+#  )
+
+  output$tblIndicadoresLondres <- DT::renderDT(
+    #df <- carga_indicadores_londres_anyos()
+    #df <- df %>% mutate(across(starts_with("var"), ~ .x / 100))
+    expr = DT::datatable(
+      carga_indicadores_londres_anyos() %>% mutate(across(starts_with("var"), ~ .x / 100)), rownames = FALSE, options = list(scrollX = TRUE,
+                                                                                 dom = 't',
+                                                                                 columnDefs = list(list(visible = FALSE, targets = c(0))))
+    ) %>% formatStyle(c('Var01','Var02','Var03','Var04','Var05','Var06','Var07','Var08','Var09','Var10'), 
+                      color = styleInterval(c(-0.0001, 0), c('red', 'black', 'limegreen'))) %>%
+      formatPercentage(c('Var01','Var02','Var03','Var04','Var05','Var06','Var07','Var08','Var09','Var10'), 2)
+  )
   
+  output$tblIndicadores <- DT::renderDT(
+    expr = DT::datatable(
+      carga_indicadores_anyos() %>% mutate(across(starts_with("var"), ~ .x / 100)), rownames = FALSE, options = list(scrollX = TRUE, 
+                                                                  columnDefs = list(list(visible=FALSE, targets=c(0))))
+    ) %>% formatStyle(c('Var01','Var02','Var03','Var04','Var05','Var06','Var07','Var08','Var09','Var10'), 
+                      color = styleInterval(c(-0.01, 0), c('red', 'black', 'limegreen'))) %>%
+      formatPercentage(c('Var01','Var02','Var03','Var04','Var05','Var06','Var07','Var08','Var09','Var10'), 2)
+  )
+  
+  ################################################################################
+  # Evolución datos por décadas
+  ################################################################################
   plot_datos_brutos_01 <- function() {
-    #filtro <- ifelse(length(input$selBorough) == 0, c("E12000007"), input$selBorough)  # Si no hay filtros, se filtra por el total de la ciudad de Londres
-    if (length(input$selBorough) == 0)
+    if (length(input$selBorough) == 0) # Si no hay filtros, se filtra por el total de la ciudad de Londres
       filtro <- c("E12000007")
     else
       filtro <- input$selBorough
     print(input$selBorough)
     print(filtro)
     g <- ggplot(carga_datos_brutos_barrios() %>% filter(Codigo %in% filtro), aes(x=Año, group=Barrio, color=Barrio)) +
-      geom_line(aes(y=Edad)) 
+      geom_line(aes(y=Edad)) +
+      theme(legend.position = "top") +
+      scale_x_discrete(name="Año", limits=seq(input$sldYear - 10,input$sldYear, by=2))
     return(g)
   }
 
   plot_datos_brutos_02 <- function() {
-    filtro <- ifelse(length(input$selBorough) == 0, "E12000007", input$selBorough)  # Si no hay filtros, se filtra por el total de la ciudad de Londres
+    if (length(input$selBorough) == 0) # Si no hay filtros, se filtra por el total de la ciudad de Londres
+      filtro <- c("E12000007")
+    else
+      filtro <- input$selBorough
     g <- ggplot(carga_datos_brutos_barrios() %>% filter(Codigo %in% filtro), aes(x=Año, group=Barrio, color=Barrio)) +
-      geom_line(aes(y=Pob_Blanca))
+      geom_line(aes(y=Pob_Blanca)) +
+      theme(legend.position = "top") +
+      scale_x_discrete(name="Año", limits=seq(input$sldYear - 10,input$sldYear, by=2))
     return(g)
   }
   
   plot_datos_brutos_03 <- function() {
-    filtro <- ifelse(length(input$selBorough) == 0, "E12000007", input$selBorough)  # Si no hay filtros, se filtra por el total de la ciudad de Londres
+    if (length(input$selBorough) == 0) # Si no hay filtros, se filtra por el total de la ciudad de Londres
+      filtro <- c("E12000007")
+    else
+      filtro <- input$selBorough
     g <- ggplot(carga_datos_brutos_barrios() %>% filter(Codigo %in% filtro), aes(x=Año, group=Barrio, color=Barrio)) +
-      geom_line(aes(y=Salario))
+      geom_line(aes(y=Salario)) +
+      theme(legend.position = "top") +
+      scale_x_discrete(name="Año", limits=seq(input$sldYear - 10,input$sldYear, by=2))
     return(g)
   }
   
   plot_datos_brutos_04 <- function() {
-    filtro <- ifelse(length(input$selBorough) == 0, "E12000007", input$selBorough)  # Si no hay filtros, se filtra por el total de la ciudad de Londres
+    test <- carga_datos_brutos_barrios()
+    print(test$Estudios)
+    if (length(input$selBorough) == 0) # Si no hay filtros, se filtra por el total de la ciudad de Londres
+      filtro <- c("E12000007")
+    else
+      filtro <- input$selBorough
     g <- ggplot(carga_datos_brutos_barrios() %>% filter(Codigo %in% filtro), aes(x=Año, group=Barrio, color=Barrio)) +
-      geom_line(aes(y=Estudios))
+      geom_line(aes(y=Estudios)) +
+      theme(legend.position = "top") +
+      scale_x_discrete(name="Año", limits=seq(input$sldYear - 10,input$sldYear, by=2))
     return(g)
   }
   
   plot_datos_brutos_05 <- function() {
-    filtro <- ifelse(length(input$selBorough) == 0, "E12000007", input$selBorough)  # Si no hay filtros, se filtra por el total de la ciudad de Londres
+    if (length(input$selBorough) == 0) # Si no hay filtros, se filtra por el total de la ciudad de Londres
+      filtro <- c("E12000007")
+    else
+      filtro <- input$selBorough
     g <- ggplot(carga_datos_brutos_barrios() %>% filter(Codigo %in% filtro), aes(x=Año, group=Barrio, color=Barrio)) +
-      geom_line(aes(y=Trafico))
+      geom_line(aes(y=Trafico)) +
+      theme(legend.position = "top") +
+      scale_x_discrete(name="Año", limits=seq(input$sldYear - 10,input$sldYear, by=2))
     return(g)
   }
   
   plot_datos_brutos_06 <- function() {
-    filtro <- ifelse(length(input$selBorough) == 0, "E12000007", input$selBorough)  # Si no hay filtros, se filtra por el total de la ciudad de Londres
+    if (length(input$selBorough) == 0) # Si no hay filtros, se filtra por el total de la ciudad de Londres
+      filtro <- c("E12000007")
+    else
+      filtro <- input$selBorough
     g <- ggplot(carga_datos_brutos_barrios() %>% filter(Codigo %in% filtro), aes(x=Año, group=Barrio, color=Barrio)) +
-      geom_line(aes(y=Esp_Vida))
+      geom_line(aes(y=Esp_Vida)) +
+      theme(legend.position = "top") +
+      scale_x_discrete(name="Año", limits=seq(input$sldYear - 10,input$sldYear, by=2))
     return(g)
   }
   
   plot_datos_brutos_07 <- function() {
-    filtro <- ifelse(length(input$selBorough) == 0, "E12000007", input$selBorough)  # Si no hay filtros, se filtra por el total de la ciudad de Londres
+    if (length(input$selBorough) == 0) # Si no hay filtros, se filtra por el total de la ciudad de Londres
+      filtro <- c("E12000007")
+    else
+      filtro <- input$selBorough
     g <- ggplot(carga_datos_brutos_barrios() %>% filter(Codigo %in% filtro), aes(x=Año, group=Barrio, color=Barrio)) +
-      geom_line(aes(y=Delitos))
+      geom_line(aes(y=Delitos)) +
+      theme(legend.position = "top") +
+      scale_x_discrete(name="Año", limits=seq(input$sldYear - 10,input$sldYear, by=2))
     return(g)
   }
   
   plot_datos_brutos_08 <- function() {
-    filtro <- ifelse(length(input$selBorough) == 0, "E12000007", input$selBorough)  # Si no hay filtros, se filtra por el total de la ciudad de Londres
+    if (length(input$selBorough) == 0) # Si no hay filtros, se filtra por el total de la ciudad de Londres
+      filtro <- c("E12000007")
+    else
+      filtro <- input$selBorough
     g <- ggplot(carga_datos_brutos_barrios() %>% filter(Codigo %in% filtro), aes(x=Año, group=Barrio, color=Barrio)) +
-      geom_line(aes(y=Servicios))
+      geom_line(aes(y=Servicios)) +
+      theme(legend.position = "top") +
+      scale_x_discrete(name="Año", limits=seq(input$sldYear - 10,input$sldYear, by=2))
     return(g)
   }
   
   plot_datos_brutos_09 <- function() {
-    filtro <- ifelse(length(input$selBorough) == 0, "E12000007", input$selBorough)  # Si no hay filtros, se filtra por el total de la ciudad de Londres
+    if (length(input$selBorough) == 0) # Si no hay filtros, se filtra por el total de la ciudad de Londres
+      filtro <- c("E12000007")
+    else
+      filtro <- input$selBorough
     g <- ggplot(carga_datos_brutos_barrios() %>% filter(Codigo %in% filtro), aes(x=Año, group=Barrio, color=Barrio)) +
-      geom_line(aes(y=Pr_Vivienda))
+      geom_line(aes(y=Pr_Vivienda)) +
+      theme(legend.position = "top") +
+      scale_x_discrete(name="Año", limits=seq(input$sldYear - 10,input$sldYear, by=2))
     return(g)
   }
   
   plot_datos_brutos_10 <- function() {
-    filtro <- ifelse(length(input$selBorough) == 0, "E12000007", input$selBorough)  # Si no hay filtros, se filtra por el total de la ciudad de Londres
+    if (length(input$selBorough) == 0) # Si no hay filtros, se filtra por el total de la ciudad de Londres
+      filtro <- c("E12000007")
+    else
+      filtro <- input$selBorough
     g <- ggplot(carga_datos_brutos_barrios() %>% filter(Codigo %in% filtro), aes(x=Año, group=Barrio, color=Barrio)) +
-      geom_line(aes(y=Alquiler))
+      geom_line(aes(y=Alquiler)) +
+      theme(legend.position = "top") +
+      scale_x_discrete(name="Año", limits=seq(input$sldYear - 10,input$sldYear, by=2))
     return(g)
   }
   
@@ -176,45 +274,47 @@ function(input, output, session) {
     plot_datos_brutos_01()
   })
 
-    output$plt_02 <- renderPlot({
-      plot_datos_brutos_02()
-    })
-    
-    output$plt_03 <- renderPlot({
-      plot_datos_brutos_03()
-    })
-    
-    output$plt_04 <- renderPlot({
-      plot_datos_brutos_04()
-    })
-    
-    output$plt_05 <- renderPlot({
-      plot_datos_brutos_05()
-    })
-    
-    output$plt_06 <- renderPlot({
-      plot_datos_brutos_06()
-    })
-    
-    output$plt_07 <- renderPlot({
-      plot_datos_brutos_07()
-    })
-    
-    output$plt_08 <- renderPlot({
-      plot_datos_brutos_08()
-    })
-    
-    output$plt_09 <- renderPlot({
-      plot_datos_brutos_09()
-    })
-    
-    output$plt_10 <- renderPlot({
-      plot_datos_brutos_10()
-    })
-    
-    # Tabla del explorador de datos
-    output$tblDatos <- DT::renderDT(expr = carga_datos_brutos_barrios(), rownames = FALSE, options = list(scrollX = TRUE, 
-                                                                                                          dom = 'tlip', 
-                                                                                                          columnDefs = list(list(visible=FALSE, targets=c(0)))))
-    
+  output$plt_02 <- renderPlot({
+    plot_datos_brutos_02()
+  })
+  
+  output$plt_03 <- renderPlot({
+    plot_datos_brutos_03()
+  })
+  
+  output$plt_04 <- renderPlot({
+    plot_datos_brutos_04()
+  })
+  
+  output$plt_05 <- renderPlot({
+    plot_datos_brutos_05()
+  })
+  
+  output$plt_06 <- renderPlot({
+    plot_datos_brutos_06()
+  })
+  
+  output$plt_07 <- renderPlot({
+    plot_datos_brutos_07()
+  })
+  
+  output$plt_08 <- renderPlot({
+    plot_datos_brutos_08()
+  })
+  
+  output$plt_09 <- renderPlot({
+    plot_datos_brutos_09()
+  })
+  
+  output$plt_10 <- renderPlot({
+    plot_datos_brutos_10()
+  })
+
+  ################################################################################
+  # Explorador de datos
+  ################################################################################
+  # Tabla del explorador de datos
+  output$tblDatos <- DT::renderDT(expr = carga_datos_brutos_anyos(), rownames = FALSE, options = list(scrollX = TRUE, 
+                                                                                                        #dom = 'tlip', 
+                                                                                                        columnDefs = list(list(visible=FALSE, targets=c(0)))))
 }
--- Visor/Visor_Gentrificacion/ui.R
+++ Visor/Visor_Gentrificacion/ui.R
@@ -37,16 +37,16 @@ sidebar <- dashboardSidebar(
     value = 2021,
     step = 10,
     ticks = FALSE
-  ),
+  )#,
   
   # Filtro por barrios
-  selectizeInput(
-    inputId = "selBorough",
-    label = "Lista de barrios (max. 3)",
-    choices = lista_barrios,
-    multiple = TRUE,
-    options = list(maxItems = 3)
-  )
+#  selectizeInput(
+#    inputId = "selBorough",
+#    label = "Lista de barrios (max. 3)",
+#    choices = lista_barrios,
+#    multiple = TRUE,
+#    options = list(maxItems = 3)
+#  )
 )
 
 body <- dashboardBody(
@@ -58,54 +58,79 @@ body <- dashboardBody(
         accordion_panel(
           title = "Gentrificación de barrios",
         ),
-        # Estado indicadores
+
+        # Análisis visual indicadores
         accordion_panel(
-          title = "Indicadores",
+          title = "Análisis visual indicadores"
+        ),
+        
+        # Explorador de indicadores
+        accordion_panel(
+          title = "Explorador de indicadores",
+          tags$h3("Indicadores ciudad de Londres"),
+          DTOutput("tblIndicadoresLondres"),
+          tags$br(),
+          tags$h3("Indicadores barrios de Londres"),
+          DTOutput("tblIndicadores")
+        ),
+        
+        # Evolución datos por décadas
+        accordion_panel(
+          title = "Evolución datos por décadas",
+
+          # Filtro por barrios
+          selectizeInput(
+            inputId = "selBorough",
+            label = "Filtro por barrios",
+            #label = "Lista de barrios (max. 3)",
+            choices = lista_barrios,
+            multiple = TRUE#,
+            #options = list(maxItems = 3)
+          ),
+          
+          
           fluidRow(
             column(6, tags$h5("Edad media")),
             column(6, tags$h5("Población raza blanca"))
           ),
           fluidRow(
-            column(6, plotOutput("plt_01"), height="300px"),
-            column(6, plotOutput("plt_02"), height="300px")
+            column(6, plotOutput("plt_01"), height="100px"),
+            column(6, plotOutput("plt_02"), height="100px")
           ),
           fluidRow(
-            column(6, tags$h5("Salario medio")),
-            column(6, tags$h5("Población con estudios superiores"))
+            column(6, tags$h5("Salario medio semanal")),
+            column(6, tags$h5("% Población con estudios superiores"))
           ),
           fluidRow(
-            column(6, plotOutput("plt_03"), height="300px"),
-            column(6, plotOutput("plt_04"), height="300px")
+            column(6, plotOutput("plt_03"), height="100px"),
+            column(6, plotOutput("plt_04"), height="100px")
           ),
           fluidRow(
             column(6, tags$h5("Volumen de tráfico")),
             column(6, tags$h5("Esperanza media de vida"))
           ),
           fluidRow(
-            column(6, plotOutput("plt_05"), height="300px"),
-            column(6, plotOutput("plt_06"), height="300px")
+            column(6, plotOutput("plt_05"), height="100px"),
+            column(6, plotOutput("plt_06"), height="100px")
           ),
           fluidRow(
             column(6, tags$h5("Media de delitos por semana")),
             column(6, tags$h5("Volumen de servicios (restaurantes, tiendas)"))
           ),
           fluidRow(
-            column(6, plotOutput("plt_07"), height="300px"),
-            column(6, plotOutput("plt_08"), height="300px")
+            column(6, plotOutput("plt_07"), height="100px"),
+            column(6, plotOutput("plt_08"), height="100px")
           ),
           fluidRow(
             column(6, tags$h5("Precio medio de la vivienda")),
             column(6, tags$h5("Precio medio del alquiler social"))
           ),
           fluidRow(
-            column(6, plotOutput("plt_09"), height="300px"),
-            column(6, plotOutput("plt_10"), height="300px")
+            column(6, plotOutput("plt_09"), height="100px"),
+            column(6, plotOutput("plt_10"), height="100px")
           )
         ),
-        # Comparador de barrios
-        accordion_panel(
-          title = "Comparador por periodos"
-        ),
+
         # Explorador de datos
         accordion_panel(
           title = "Explorador de datos",
